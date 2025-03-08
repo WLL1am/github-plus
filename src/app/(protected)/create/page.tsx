0bd69b8fb4d71f4da52e88a001e36c4875edb4fd
William Chung <118134309+WLL1am@users.noreply.github.com>
2025-03-07 20:14:06 -0800

    created project model, linking repositories

--- src/app/(protected)/create/page.tsx
+++ src/app/(protected)/create/page.tsx
@@ -1,9 +1,11 @@
 'use client'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
+import { api } from '@/trpc/react'
 import Image from 'next/image'
 import React from 'react'
 import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
 
 type FormInput = {
     repoUrl: string
@@ -13,9 +15,22 @@ type FormInput = {
 
 const CreatePage = () => {
     const { register, handleSubmit, reset } = useForm<FormInput>()
+    const createProject = api.project.createProject.useMutation()
 
     function onSubmit(data: FormInput) {
-        window.alert(JSON.stringify(data, null, 2))
+        createProject.mutate({
+            githubUrl: data.repoUrl,
+            name: data.projectName,
+            githubToken: data.githubToken
+        }, {
+            onSuccess: () => {
+                toast.success('Project created successfully')
+                reset()
+            },
+            onError: () => {
+                toast.error('Failed to create project')
+            }
+        })
         return true
     }
   return (
@@ -51,7 +66,7 @@ const CreatePage = () => {
                         placeholder='GitHub Token (Optional)'
                     />
                     <div className='h-4'></div>
-                    <Button type='submit'>
+                    <Button type='submit' disabled={createProject.isPending}>
                         Create Project
                     </Button>
                 </form>
