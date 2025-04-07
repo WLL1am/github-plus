7264cdb5896a3a9b64a6c034225e966c59c1e1fa
William Chung <118134309+WLL1am@users.noreply.github.com>
2025-04-07 16:13:58 -0700

    implemented functions to add and retrieve meetings

--- src/app/(protected)/dashboard/meeting-card.tsx
+++ src/app/(protected)/dashboard/meeting-card.tsx
@@ -6,10 +6,17 @@ import React from 'react'
 import { uploadFile } from '@/lib/firebase'
 import { Presentation, Upload } from 'lucide-react'
 import { Button } from '@/components/ui/button'
+import { api } from '@/trpc/react'
+import useProject from '@/hooks/use-project'
+import { toast } from 'sonner'
+import { useRouter } from 'next/navigation'
 
 const MeetingCard = () => {
+    const { project } = useProject()
     const [ isUploading, setIsUploading ] = React.useState(false)
     const [ progress, setProgress ] = React.useState(0)
+    const router = useRouter()
+    const uploadMeeting = api.project.uploadMeeting.useMutation()
     const { getRootProps, getInputProps } = useDropzone({
         accept: {
             'audio/*': ['.mp3', '.wav', '.m4a']
@@ -17,11 +24,25 @@ const MeetingCard = () => {
         multiple: false,
         maxSize: 50_000_000, 
         onDrop: async acceptedFiles => {
+            if (!project) return
             setIsUploading(true)
-            console.log(acceptedFiles)
             const file = acceptedFiles[0]
-            const downloadURL = await uploadFile(file as File, setProgress)
-            window.alert(downloadURL)
+            if (!file) return
+            const downloadURL = await uploadFile(file as File, setProgress) as string
+            uploadMeeting.mutate({
+                projectId: project.id,
+                meetingUrl: downloadURL,
+                name: file.name
+            }, {
+                onSuccess: () => {
+                    toast.success("Meeting uploaded successfully")
+                    router.push('/meetings')
+                },
+                onError: () => {
+                    toast.error("Failed to upload meeting")
+                }
+            })
+
             setIsUploading(false)
         }
     })
