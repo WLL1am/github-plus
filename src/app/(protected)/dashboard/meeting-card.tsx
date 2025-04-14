433777908df169ff83a9d65e445e727b62c7ed9a
William Chung <118134309+WLL1am@users.noreply.github.com>
2025-04-13 22:14:37 -0700

    function to process and delete meetings

--- src/app/(protected)/dashboard/meeting-card.tsx
+++ src/app/(protected)/dashboard/meeting-card.tsx
@@ -10,9 +10,16 @@ import { api } from '@/trpc/react'
 import useProject from '@/hooks/use-project'
 import { toast } from 'sonner'
 import { useRouter } from 'next/navigation'
+import { useMutation } from '@tanstack/react-query'
+import axios from 'axios'
 
 const MeetingCard = () => {
     const { project } = useProject()
+    const processMeeting = useMutation({ mutationFn: async (data: { meetingUrl: string, meetingId: string, projectId: string }) => {
+        const { meetingUrl, meetingId, projectId } = data
+        const response = await axios.post('/api/process-meeting', { meetingUrl, meetingId, projectId })
+        return response.data
+    }})
     const [ isUploading, setIsUploading ] = React.useState(false)
     const [ progress, setProgress ] = React.useState(0)
     const router = useRouter()
@@ -34,9 +41,10 @@ const MeetingCard = () => {
                 meetingUrl: downloadURL,
                 name: file.name
             }, {
-                onSuccess: () => {
+                onSuccess: (meeting) => {
                     toast.success("Meeting uploaded successfully")
                     router.push('/meetings')
+                    processMeeting.mutateAsync({ meetingUrl: downloadURL, meetingId: uploadMeeting.data!.id, projectId: project.id })
                 },
                 onError: () => {
                     toast.error("Failed to upload meeting")
--- src/server/api/routers/project.ts
+++ src/server/api/routers/project.ts
@@ -87,8 +87,12 @@ export const projectRouter = createTRPCRouter({
                 status: "PROCESSING"
             }
         })
+        return meeting
     }),
     getMeetings: protectedProcedure.input(z.object({ projectId: z.string() })).query(async ({ ctx, input }) => {
         return await ctx.db.meeting.findMany({ where: { projectId: input.projectId }, include: { issues: true } })
+    }),
+    deleteMeeting: protectedProcedure.input(z.object({ meetingId: z.string() })).mutation(async ({ ctx, input }) => {
+        return await ctx.db.meeting.delete({ where: { id: input.meetingId }})
     })
 })
