76903e036e2bc32c3e1f9d48daa4d2842c3d0700
William Chung <118134309+WLL1am@users.noreply.github.com>
2025-04-16 19:03:09 -0700

    implemented archive button for projects

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -7,6 +7,7 @@ import React from 'react'
 import CommitLog from './commit-log'
 import AskQuestionCard from './ask-question-card'
 import MeetingCard from './meeting-card'
+import ArchiveButton from './archive-button'
 
 const DashboardPage = () => {
   const { project } = useProject()
@@ -35,7 +36,7 @@ const DashboardPage = () => {
         <div className="flex items-center gap-4">
           TeamMembers
           InviteButton
-          ArchiveButton
+          <ArchiveButton />
         </div>
       </div>
 
