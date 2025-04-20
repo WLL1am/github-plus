019c17d358eb7cdb52132b39ed9fb9233b2f6b9d
William Chung <118134309+WLL1am@users.noreply.github.com>
2025-04-19 21:19:50 -0700

    validating and updating credits to create projects

--- src/app/(protected)/create/page.tsx
+++ src/app/(protected)/create/page.tsx
@@ -3,6 +3,7 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import useRefetch from '@/hooks/use-refetch'
 import { api } from '@/trpc/react'
+import { Info } from 'lucide-react'
 import Image from 'next/image'
 import React from 'react'
 import { useForm } from 'react-hook-form'
@@ -17,25 +18,35 @@ type FormInput = {
 const CreatePage = () => {
     const { register, handleSubmit, reset } = useForm<FormInput>()
     const createProject = api.project.createProject.useMutation()
+    const checkCredits = api.project.checkCredits.useMutation()
     const refetch = useRefetch()
 
     function onSubmit(data: FormInput) {
-        createProject.mutate({
-            githubUrl: data.repoUrl,
-            name: data.projectName,
-            githubToken: data.githubToken
-        }, {
-            onSuccess: () => {
-                toast.success('Project created successfully')
-                refetch()
-                reset()
-            },
-            onError: () => {
-                toast.error('Failed to create project')
-            }
-        })
-        return true
+        if (!!checkCredits.data) {
+            createProject.mutate({
+                githubUrl: data.repoUrl,
+                name: data.projectName,
+                githubToken: data.githubToken
+            }, {
+                onSuccess: () => {
+                    toast.success('Project created successfully')
+                    refetch()
+                    reset()
+                },
+                onError: () => {
+                    toast.error('Failed to create project')
+                }
+            })
+        } else {
+            checkCredits.mutate({
+                githubUrl: data.repoUrl,
+                githubToken: data.githubToken
+            })
+        }
     }
+
+    const hasEnoughCredits = checkCredits?.data?.userCredits ? checkCredits.data.fileCount <= checkCredits.data.userCredits : true
+
   return (
     <div className='flex items-center gap-12 h-full justify-center'>
         <Image src='/undraw_github.png' alt='' width={350} height={350}/>
@@ -68,9 +79,22 @@ const CreatePage = () => {
                         {...register('githubToken')}
                         placeholder='GitHub Token (Optional)'
                     />
+                    {!!checkCredits.data && (
+                        <>
+                            <div className="mt-4 bg-orange-50 px-4 py-2 rounded-md border border-orange-200 text-orange-700">
+                                <div className="flex items-center gap-2">
+                                    <Info className='size-4' />
+                                    <p className='text-sm'>You will be charged <strong>{checkCredits.data?.fileCount}</strong> credits for this repository.</p>
+                                </div>
+                                <p className='text-sm text-blue-600 ml-6'>You have <strong>{checkCredits.data?.userCredits}</strong> credits remaining.</p>
+
+                            </div>
+                        </>
+                    )}
+
                     <div className='h-4'></div>
-                    <Button type='submit' disabled={createProject.isPending}>
-                        Create Project
+                    <Button type='submit' disabled={createProject.isPending || !!checkCredits.isPending || !hasEnoughCredits}>
+                        {!!checkCredits.data ? 'Create Project' : "check Credits"}
                     </Button>
                 </form>
             </div>
--- src/server/api/routers/project.ts
+++ src/server/api/routers/project.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
 import { pollCommits } from "@/lib/github";
-import { indexGithubRepo } from "@/lib/github-loader";
+import { checkCredits, indexGithubRepo } from "@/lib/github-loader";
 import { Input } from "postcss";
 
 
@@ -13,6 +13,17 @@ export const projectRouter = createTRPCRouter({
             githubToken: z.string().optional()
         })
     ).mutation(async ({ctx, input}) => {
+        const user = await ctx.db.user.findUnique({ where: { id: ctx.user.userId! }, select: { credits: true } })
+        if (!user) {
+            throw new Error('User not found')
+        }
+        const currentCredits = user.credits || 0
+        const fileCount = await checkCredits(input.githubUrl, input.githubToken)
+
+        if (currentCredits < fileCount) {
+            throw new Error('Insufficient credits')
+        }
+
         const project = await ctx.db.project.create({
             data: {
                 githubUrl: input.githubUrl,
@@ -27,6 +38,7 @@ export const projectRouter = createTRPCRouter({
         })
         await indexGithubRepo(project.id, input.githubUrl, input.githubToken)
         await pollCommits(project.id)
+        await ctx.db.user.update({ where: { id: ctx.user.userId! }, data: { credits: { decrement: fileCount } } })
         return project
     }),
     getProjects: protectedProcedure.query(async ({ ctx }) => {
@@ -106,5 +118,10 @@ export const projectRouter = createTRPCRouter({
     }),
     getMyCredits: protectedProcedure.query(async ({ ctx }) => {
         return await ctx.db.user.findUnique({ where: { id: ctx.user.userId! }, select: { credits: true } })
+    }),
+    checkCredits: protectedProcedure.input(z.object({ githubUrl: z.string(), githubToken: z.string().optional() })).mutation(async ({ctx, input}) => {
+        const fileCount = await checkCredits(input.githubUrl, input.githubToken)
+        const userCredits = await ctx.db.user.findUnique({ where: { id: ctx.user.userId! }, select: { credits: true } })
+        return { fileCount, userCredits: userCredits?.credits || 0}
     })
 })
